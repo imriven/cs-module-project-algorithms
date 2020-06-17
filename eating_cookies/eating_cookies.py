0c24ea345093032f63641552e1c1083c944027a6
Christina Harris <christinathedesigner@outlook.com>
2020-06-17 15:04:32 -0700

    finished mvp thursday

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -2,13 +2,21 @@
 Input: an integer
 Returns: an integer
 '''
-def eating_cookies(n):
-    # Your code here
+cookies = [1, 2, 3]
 
-    pass
+def eating_cookies(n):
+    if n == 0:
+        return 1
+    if n < 0:
+        return 0
+    count = 0
+    for i in range(len(cookies)):
+        count += eating_cookies(n - cookies[i])
+    return count
+
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
-    num_cookies = 5
+    num_cookies = 30
 
     print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")
--- sliding_window_max/sliding_window_max.py
+++ sliding_window_max/sliding_window_max.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 '''
 Input: a List of integers as well as an integer `k` representing the size of the sliding window
 Returns: a List of integers
@@ -14,10 +16,20 @@ Returns: a List of integers
  #THURSDAY
 
 def sliding_window_max(nums, k):
+    q = deque()
     results = []
-    for i, num in enumerate(nums):
-        if i + k <= len(nums):
-            results.append(max(nums[i:i + k]))
+    for i in range(k):
+        while q and nums[i] >= nums[q[-1]]:
+            q.pop()
+        q.append(i)
+    for i in range(k, len(nums)):
+        results.append(nums[q[0]])
+        while q and q[0] <= i - k:
+            q.popleft()
+        while q and nums[i] >= nums[q[-1]]:
+            q.pop()
+        q.append(i)
+    results.append(nums[q[0]])
     return results
 
 
