d5d9019c0bfd7a2f97ce8fedcf54581542f2cba3
Christina Harris <christinathedesigner@outlook.com>
2020-06-18 16:29:20 -0700

    reworked eating cookies

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -1,9 +1,21 @@
+import sys
+sys.setrecursionlimit(5000)
 '''
 Input: an integer
 Returns: an integer
 '''
 cookies = [1, 2, 3]
 
+def memoize(f):
+    cache = {}
+
+    def wrapped(n):
+        if n not in cache:
+            cache[n] = f(n)
+        return cache[n]
+    return wrapped
+
+@memoize
 def eating_cookies(n):
     if n == 0:
         return 1
@@ -17,6 +29,6 @@ def eating_cookies(n):
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
-    num_cookies = 30
+    num_cookies = 500
 
     print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")
